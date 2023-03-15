87ae4b0382552853d8b5023a60c0e8af66d72d67
henry-49 <henryswebdevelopment@gmail.com>
2023-03-15 22:18:20 +0100

    add jQuery function to display image on upload

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -2,6 +2,8 @@
 
 @section('admin')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
 <div class="page-content">
     <div class="container-fluid">
             <div class="row">
@@ -46,9 +48,9 @@
                                     <!-- end row -->
 
                                      <div class="row mb-3">
-                                        <label for="profile_image" class="col-sm-2 col-form-label"></label>
+                                        <label for="ShowImage" class="col-sm-2 col-form-label"></label>
                                         <div class="col-sm-10">
-                                             <img class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                                             <img id="ShowImage" class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
                                         </div>
                                     </div>
                                     <!-- end row -->
@@ -66,4 +68,16 @@
     </div>
 </div>
 
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#profile_image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+               $('#ShowImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 @endsection
