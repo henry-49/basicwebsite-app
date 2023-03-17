a0fa9ce6057f522edfaff98cd13fd92b70fa16a6
henry-49 <henryswebdevelopment@gmail.com>
2023-03-17 13:34:30 +0100

    create homeslide model and migration for backend

---
+++ app/Models/HomeSlide.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HomeSlide extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     *
+     */
+
+        protected $guarded = [];
+
+    // protected $fillable = [
+    //     'title',
+    //     'short_title',
+    //     'home_slide',
+    //     'video_url',
+
+    // ];
+}
